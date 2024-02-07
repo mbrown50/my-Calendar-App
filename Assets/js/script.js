b102087b868b98f739fe5164f596250b2565a33b
M <melissabrown@Melissas-MacBook-Pro.local>
2024-02-07 15:48:35 -0600

    shows local stored descriptions

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -4,14 +4,14 @@
 $(function () {
 
   let curDay = dayjs();
-  console.log(curDay);
+  //console.log(curDay);
 
   // TODO: Add code to display the current date in the header of the page.
   document.getElementById('currentDay').innerHTML = curDay.$d;
 
-  let showTasks = function (element) { // "this" div class time-block
+  let showTasks = function (element) { // element is div class time-block
 
-    console.log("THIS: " + this);
+   // console.log("THIS: " + this);
     console.log("element: " + element);
 
     /* -- div class time-block
@@ -24,8 +24,26 @@ $(function () {
     </div>
     */
 
-    element.getElementsByTagName('textarea') //[0].innerHTML("myTask")
-    console.log("myTasks");
+    let time_block_id = element.id;
+    console.log("show time block ID: " + time_block_id);
+
+    let textTag = element.getElementsByTagName('textarea')[0]; //.innerHTML("myTask")
+    console.log(textTag);
+
+    // test 
+     //localStorage.setItem("hour-9", "Wake up.");
+    
+     let description = localStorage.getItem(time_block_id);
+      //console.log("description :" + description);
+
+     if (description !== null)
+     {
+      //console.log("show: " + description);
+      //console.log("text: " + textTag);
+      //console.log(textTag.innerHTML);
+      textTag.innerHTML = description;
+     }
+     
   }
 
   // TODO: Add a listener for click events on the save button. This code should
@@ -34,7 +52,7 @@ $(function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-  let mySave = function (element) { // "this" is button class saveBtn
+  let mySave = function (element) { // element is div class time-block
 
       /* -- div class time-block
     <div id="hour-9" class="row time-block ">
@@ -46,14 +64,14 @@ $(function () {
     </div>
     */
 
-    console.log("THIS: " + this);
-    console.log("element: " + element);
+    //console.log("THIS: " + this);
+    //console.log("element: " + element);
 
     //let time_block_id = element.closest('.time-block')//.attr('id');
     //console.log(time_block_id)
 
     let time_id = element.getElementsByTagName('div')[0]; //.attr('class');
-    console.log(time_id);
+   // console.log(time_id);
 
     // TODO: Add code to get any user input that was saved in localStorage and set
     // the values of the corresponding textarea elements. HINT: How can the id
@@ -69,12 +87,15 @@ $(function () {
     </div>
     */
 
+     // test 
+     //localStorage.setItem("hour-9", "Wake up.");
+
   };
 
-  let setHourColors = function (element) { // "this" is div class time-block
+  let setHourColors = function (element) { // // element is div class time-block
 
-    console.log("THIS: " + this);
-    console.log("element: " + element);
+   // console.log("THIS: " + this);
+    //console.log("element: " + element);
 
     // TODO: Add code to apply the past, present, or future class to each time
     // block by comparing the id to the current hour. HINTS: How can the id
@@ -91,50 +112,51 @@ $(function () {
       </button>
     </div>
     */
-
+    curDay = dayjs();
     // TODO: Add code to display the current date in the header of the page.
     document.getElementById('currentDay').innerHTML = curDay.$d;
-
-    curDay = dayjs();
-
+   
     //let time_block = element.closest('.time-block');
     let time_block_id = element.id;
-    console.log("time_block_id: " + time_block_id)
+    //console.log("time_block_id: " + time_block_id)
 
-    let time_id = element.getElementsByTagName('div')[0];
+    
+   // let time_id = element.getElementsByTagName('div')[0];
+    //console.log("time_id: " + time_id.id)
 
-    console.log("time_id: " + time_id.id)
+   // let calHour = time_id.innerHTML;
+    time_block_id = Number(time_block_id.replace("hour-", ""));
+    //console.log("calHour: " + calHour);
 
-    let calHour = time_id.innerHTML;
+    curHour = curDay.format("HH")
+    //console.log("dayjs :" + curHour);
 
-
-    time_block_id = time_block_id.replace("hour-", "");
-
-    console.log("calHour: " + calHour);
-
-    curHour = curDay.$H;
-
+    /*
     if (curHour > 12) {
       curHour = curHour + 12;
     }
+    */
 
-    console.log(curDay.$H);
-    console.log("curHour: " + curHour);
+    //console.log(curDay.$H);
+    //console.log("curHour: " + curHour);
 
     if (time_block_id < curHour) {
       element.classList.add("past");
       element.classList.remove("present");
       element.classList.remove("future");
+      //console.log("grey");
     }
     else if (time_block_id == curHour) {
       element.classList.remove("past");
       element.classList.add("present");
       element.classList.remove("future");
+      //console.log("red")
     }
     else if (time_block_id > curHour) {
       element.classList.remove("past");
       element.classList.remove("present");
       element.classList.add("future");
+      //console.log("green")
     }
     else {
       alert("error in hours");
@@ -148,14 +170,14 @@ $(function () {
 
   for (const element of document.getElementsByClassName("time-block")) {
     
-    console.log("THIS loop: " + this);
-    console.log("element loop: " + element)
+    //console.log("THIS loop: " + this);
+    //console.log("element loop: " + element)
 
     setHourColors(element);
     showTasks(element);
     let saveButton = element.getElementsByClassName('saveBtn')[0].addEventListener('click', () => mySave(element));
     
-    console.log("saveButton: " + saveButton);
+   // console.log("saveButton: " + saveButton);
 
   }
 
